b7d9798db754bbd6b9a4e8930fc8ea574c80ee9c
Murat Dere <muratdere90@gmail.com>
2024-01-20 22:33:33 +0300

    add-user-end-paint-to-identity-server

---
+++ backend/IdentityServer/WalletService.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using WalletService.IdentityServer.Models;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace WalletService.IdentityServer.Controllers
+{
+
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(m => m.Type == JwtRegisteredClaimNames.Sub);
+            if (userIdClaim == null)
+                return BadRequest();
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+            if (user == null) return BadRequest();
+            return Ok(new { user.Id, user.Name, user.LastName });
+        }
+    }
+}
