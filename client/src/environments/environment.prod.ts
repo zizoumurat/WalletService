5534250e6a5c5a970f22f43a47306a5808837ae4
Murat Dere <muratdere90@gmail.com>
2024-01-23 15:37:24 +0300

    bug-fix-added-url-to-environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'http://localhost:6001/api/',
-  hubUrl: 'http://localhost:4003/notificationhub'
+  identityUrl: 'http://localhost:5000/',
+  hubUrl: 'http://localhost:4003/hub'
 };
